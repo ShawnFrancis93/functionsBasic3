f44c5007643e8b0970f261b5e42db613dfd70646
Shawn <shawnpfrancis1993@gmail.com>
2021-05-07 07:56:10 -0400

    Plan to finish today

---
+++ basic3.py
@@ -0,0 +1,20 @@
+x = [ [5,2,3], [10,8,9] ] 
+x[1][0] = '15'
+print(x)
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+def changeName(listname):
+    for x in range(len(listname)):
+        print(students[0] )
+
+changeName(students)
+
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
